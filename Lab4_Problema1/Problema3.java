1cd0cdd1f1ee79ba30ffbf8f7078d930be778340
andasoare <61412200+andasoare@users.noreply.github.com>
2020-05-20 16:04:30 +0300

    Add files via upload

---
+++ Lab4_Problema1/Problema3.java
@@ -0,0 +1,43 @@
+package Lab4_Problema1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problema3 {
+    public static void main(String[] args) {
+
+        int [] arr =  new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Numar ce se afla in jumatatea din stanga, pe pozitie para: " + searchInt(arr, 3));
+        System.out.println("Numar ce se afla in  jumatatea din stanga, pe pozitie impara: " + searchInt(arr, 4));
+
+        System.out.println("Numar ce se afla in jumatatea din dreapta, pe pozitie para: " + searchInt(arr, 7));
+        System.out.println("Numar ce se afla in jumatatea din dreapta, pe pozitie impara: " + searchInt(arr, 8));
+
+        System.out.println("Numar aflat in mijlocul sirului: " + searchInt(arr, 5));
+
+        System.out.println("Primul numar: " + searchInt(arr, 1));
+        System.out.println("Ultimul numar: " + searchInt(arr, 10));
+
+        System.out.println("Numar inexistent: " + searchInt(arr, 13));
+
+    }
+
+    static boolean searchInt (int[] intArr, int toBeSearched) {
+        int left = 0, right = intArr.length - 1;
+
+        while(left <= right)
+        {
+            int i = (left + right) / 2;
+            if(intArr[i] == toBeSearched)
+            {
+                return true;
+            }
+            if(intArr[i] > toBeSearched)
+                right = i - 1;
+            if(intArr[i] < toBeSearched)
+                left = i + 1;
+        }
+        return false;
+    }
+}
